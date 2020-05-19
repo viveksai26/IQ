31979876a7f035f6a9595727b277441eaf9555f0
viveksai26 <viveksai26@gmail.com>
2020-05-19 19:37:48 +0530

    Added Answers

--- src/routes/private/answers.js
+++ src/routes/private/answers.js
@@ -17,7 +17,9 @@ router.post('/answerByUserId', async function (req, res, next) {
             if (err) {
                 next(err)
             }
-            connection.query('call answers_by_userid(' + req.body.userId + ')', function (err, result) {
+            console.log(req.body.userId);
+
+            connection.query('call answers_by_userid(' + '"' + req.body.userId + '"' + ')', function (err, result) {
                 connection.release();
                 if (err) {
                     next(err)
@@ -47,7 +49,38 @@ router.post('/answerAdd', async function (req, res, next) {
             if (err) {
                 next(err)
             }
-            connection.query('call answers_add(' + req.body.userId + ')', function (err, result) {
+            connection.query('call answers_add(' + '"' + req.body.userId + '"' + ',' + '"' + req.body.questionId + '"' + ',' + '"' + req.body.answer + '"' + ')', function (err, result) {
+                connection.release();
+                if (err) {
+                    next(err)
+                    return
+                }
+                try {
+                    buildStatus(res, HTTPCodes.SUCCESS, result)
+                } catch (err) {
+                    next(err)
+                }
+
+            })
+            connection.on('error', function (error) {
+                next(error)
+            })
+        })
+    } catch (err) {
+        next(err)
+    }
+
+})
+
+router.post('/answerDeleteByUserId', async function (req, res, next) {
+    let pool;
+    try {
+        pool = await connection(config)
+        pool.getConnection(function (err, connection) {
+            if (err) {
+                next(err)
+            }
+            connection.query('call answers_user_delete(' + '"' + req.body.answerId + '"' + ')', function (err, result) {
                 connection.release();
                 if (err) {
                     next(err)
